46a51a1ec1d0b0f5cb03a7bc4df5349b0e2526e6
Kevin Molina <kevinmolina.dev@gmail.com>
2023-01-02 15:46:03 -0400

    Añadiendo mejoras => Agregando contenido al html, css y js

---
+++ index.js
@@ -0,0 +1,8 @@
+/*Aprendiendo DOM
+Para poder modificar los elementos html con js debemos llamarlos mediante clases, id o por las etiquetas de los mismas*/
+const titulo = document.getElementById("title");
+const titulo_2 = document.getElementById("titleTwo");
+console.log(`El titulo es: ${titulo.innerText}`);
+console.log(`El titulo 2 es: ${titulo_2.textContent}`);
+
+
